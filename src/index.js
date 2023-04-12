eb6f21b99bef9b5add1db43d9001de3a0fb49601
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-04-12 15:23:03 +0200

    fix: Show/hide elements based on selected options

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ export default function ({
     pointsPossible,
     gradingType,
     gradingStandardId,
-    submissionType,
-    omitFromFinalGrade
+    omitFromFinalGrade,
+    submissionType
 }) {
     router.onRoute('courses.assignments.new', async () => {
         await dom.onElementReady('#edit_assignment_wrapper');
@@ -23,16 +23,39 @@ export default function ({
         // Select grading type if defined
         if (typeof gradingType === 'string') {
             const select = document.getElementById('assignment_grading_type');
+            const gradedAssignmentFields = document.getElementById('graded_assignment_fields');
+            const viewGradingLevels = document.getElementById('view-grading-levels');
 
             select.value = gradingType;
 
-            // Set grading standard ID if defined
-            if ((gradingType === 'letter_grade' || gradingType === 'gpa_scale')
-                && typeof gradingStandardId === 'number') {
-                const input = document.getElementsByName('grading_standard_id')[0];
+            switch (gradingType) {
+                case 'letter_grade':
+                case 'gpa_scale':
+                    gradedAssignmentFields.style.display = 'block';
+                    viewGradingLevels.style.display = 'block';
 
-                input.value = gradingStandardId;
-            };
+                    // Set grading standard ID if defined
+                    if (typeof gradingStandardId === 'number') {
+                        const input = document.getElementsByName('grading_standard_id')[0];
+
+                        input.value = gradingStandardId;
+                    };
+
+                    break;
+
+                default:
+                    gradedAssignmentFields.style.display = 'block';
+                    viewGradingLevels.style.display = 'none';
+
+                    break;
+
+                case 'not_graded':
+                    gradedAssignmentFields.style.display = 'none';
+                    viewGradingLevels.style.display = 'none';
+
+                    // Stop here if assignment is not graded
+                    return;
+            }
         };
 
         // Select submission type if defined
@@ -40,10 +63,41 @@ export default function ({
             const select = document.getElementById('assignment_submission_type');
             const onlineSubmissionTypes = document.getElementById('assignment_online_submission_types');
             const externalToolSettings = document.getElementById('assignment_external_tool_settings');
+            const allowedAttemptsFields = document.getElementById('allowed_attempts_fields');
+            const groupCategorySelector = document.getElementById('group_category_selector');
+            const peerReviewsFields = document.getElementById('assignment_peer_reviews_fields');
 
             select.value = submissionType;
-            onlineSubmissionTypes.style.display = (submissionType === 'online') ? 'block' : 'none';
-            externalToolSettings.style.display = (submissionType === 'external_tool') ? 'block' : 'none';
+
+            switch (submissionType) {
+                case 'none':
+                case 'on_paper':
+                    onlineSubmissionTypes.style.display = 'none';
+                    externalToolSettings.style.display = 'none';
+                    allowedAttemptsFields.style.display = 'none';
+                    groupCategorySelector.style.display = 'block';
+                    peerReviewsFields.style.display = 'block';
+
+                    break;
+
+                case 'online':
+                    onlineSubmissionTypes.style.display = 'block';
+                    externalToolSettings.style.display = 'none';
+                    allowedAttemptsFields.style.display = 'block';
+                    groupCategorySelector.style.display = 'block';
+                    peerReviewsFields.style.display = 'block';
+
+                    break;
+
+                case 'external_tool':
+                    onlineSubmissionTypes.style.display = 'none';
+                    externalToolSettings.style.display = 'block';
+                    allowedAttemptsFields.style.display = 'block';
+                    groupCategorySelector.style.display = 'none';
+                    peerReviewsFields.style.display = 'none';
+
+                    break;
+            }
         };
 
         // Set whether to omit the grade from the final grade
